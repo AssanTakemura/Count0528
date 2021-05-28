cc0751a63990969658e2a98701e9035e9f36bd3f
fummicc1 <fumiya.tennis1@gmail.com>
2021-05-28 18:12:17 +0900

    Add Speech code.

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -10,10 +10,16 @@
 
 
 import UIKit
+import Speech
 
 class ViewController: UIViewController {
     
     var number: Int = 0
+    
+    let speechRecognizer: SFSpeechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "ja-JP"))!
+    let audioEngine: AVAudioEngine = AVAudioEngine()
+    var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    var recognitionTask: SFSpeechRecognitionTask?
     
     //問題
     @IBOutlet var randomlabel: UILabel!
@@ -24,17 +30,72 @@ class ViewController: UIViewController {
     @IBOutlet var label3: UILabel!
     @IBOutlet var label4: UILabel!
     
+    var number1: Int?
+    var number2: Int?
+    var number3: Int?
+    var number4: Int?
+    
 
     let randomInt = Int.random(in: 4..<100)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        SFSpeechRecognizer.requestAuthorization { status in
+            print("isAuthorized: \(status == .authorized)")
+        }
     }
     
     
     @IBAction func Start(){
+        try! startRecording()
+    }
+    
+    // 音声認識の開始
+    func startRecording() throws {
         
+        let inputNode = audioEngine.inputNode
+        let format = inputNode.inputFormat(forBus: 0)
+        
+        // マイクの入力受信
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: format) { buffer, time in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        // 開始
+        audioEngine.prepare()
+        try audioEngine.start()
+        
+        setupSpeech()
+    }
+    
+    // 音声→文字の処理
+    func setupSpeech() {
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("クラッシュ")
+        }
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest, resultHandler: { result, error in
+            
+            var isFinal = false
+            
+            if let result = result {
+                // ラベルに文字起こしの結果を表示
+                self.number1 = Int(result.bestTranscription.formattedString)
+                isFinal = result.isFinal
+            }
+            
+            if error != nil || isFinal {
+                // エラーがある or 音声認識が終了した
+                
+                self.audioEngine.stop()
+                self.audioEngine.inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+            }
+        })
     }
     
 
